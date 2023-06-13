487694e8f2c103f1ba9cc932adadffa462586dd2
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2023-06-13 23:37:43 +0300

    Build a pyramid-shaped tower

---
+++ python/13-build_tower.py
@@ -0,0 +1,12 @@
+"""
+    tower_builder - build a pyramid-shaped tower
+    @n_floors: number of floor
+"""
+
+def tower_builder(n_floors):
+    tower = []
+    for floor in range(n_floors):
+        spaces = ' ' * (n_floors - floor - 1)
+        blocks = '*' * (2 * floor + 1)
+        tower.append(space + blocks + spaces)
+    return tower
