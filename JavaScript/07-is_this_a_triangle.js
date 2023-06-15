1524fff10b1eeff127c4946f4101072d9316e5b0
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2023-06-16 00:28:29 +0300

    refactor the if condition to get desire result and also add function documentation

--- JavaScript/07-is_this_a_triangle.js
+++ JavaScript/07-is_this_a_triangle.js
@@ -1,7 +1,15 @@
-function isTriangle(a, b, c) {
-  if (c > a + b || b > c + a || a > b + c) {
-    return false;
-  } else {
+/**
+ * isTriangle - checks if a triangle can be built from
+ *              a given sides
+ * @a: side to check
+ * @b: side to check
+ * @c: side to check
+ * return: true if the triangle can be built else false if not
+ */
+const isTriangle = (a, b, c) => {
+  if (c < a + b && b < c + a && a < b + c) {
     return true;
+  } else {
+    return false;
   }
-}
+};
