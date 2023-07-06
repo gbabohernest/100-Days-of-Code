55c0a300ac5ba6e1d19176351c043630fa784e35
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2023-07-06 23:53:32 +0300

    basic implementation of the toggle theme switcher

--- Practice Projects/calculator-app-main/app/js/theme.js
+++ Practice Projects/calculator-app-main/app/js/theme.js
@@ -8,17 +8,44 @@ console.log(buttons);
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const target = e.target;
-    changeBg(target);
+    selectTheme(target);
     console.log(target.className);
   });
 });
 
-function changeBg(theme) {
-  if (theme.className === "2") {
-    body.style.backgroundColor = "red";
-  } else if (theme.className === "3") {
-    body.style.backgroundColor = "green";
-  } else {
-    body.style.backgroundColor = "blue";
+function selectTheme(themeBtn) {
+  if (themeBtn.className == "2") {
+    if (
+      calculatorHeader.classList.contains("theme-1") ||
+      calculatorHeader.classList.contains("theme-3")
+    ) {
+      calculatorHeader.classList.remove("theme-1");
+      calculatorHeader.classList.remove("theme-3");
+      calculatorHeader.classList.add("theme-2");
+      body.style.backgroundColor = "red";
+    }
+  }
+
+  if (themeBtn.className == "3") {
+    if (
+      calculatorHeader.classList.contains("theme-1") ||
+      calculatorHeader.classList.contains("theme-2")
+    ) {
+      calculatorHeader.classList.remove("theme-1");
+      calculatorHeader.classList.remove("theme-2");
+      calculatorHeader.classList.add("theme-3");
+      body.style.backgroundColor = "orange";
+    }
+  }
+  if (themeBtn.className == "1") {
+    if (
+      calculatorHeader.classList.contains("theme-2") ||
+      calculatorHeader.classList.contains("theme-3")
+    ) {
+      calculatorHeader.classList.remove("theme-2");
+      calculatorHeader.classList.remove("theme-3");
+      calculatorHeader.classList.add("theme-1");
+      body.style.backgroundColor = "green";
+    }
   }
 }
