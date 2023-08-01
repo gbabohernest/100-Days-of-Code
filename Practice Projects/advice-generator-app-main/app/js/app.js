93cb3a5734819dc89d6361316a08d5a7bd80e34a
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2023-08-01 21:59:55 +0300

    fetch advice data and update UI

--- Practice Projects/advice-generator-app-main/app/js/app.js
+++ Practice Projects/advice-generator-app-main/app/js/app.js
@@ -1,2 +1,29 @@
 const adviceNum = document.querySelector('#adviceNum');
-const advice = document.querySelector('#advice');
+const adviceContent = document.querySelector('#advice');
+const btn = document.querySelector('.advice__button img');
+
+const adviceSlipAPI = 'https://api.adviceslip.com/advice';
+
+const fetchAdvice = async () => {
+  const timestamp = Date.now();
+  const urlWithTimestamp = `${adviceSlipAPI}?timestamp=${timestamp}`;
+  try {
+    const response = await fetch(urlWithTimestamp);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('Sorry, Can not fetch advice!!');
+  }
+};
+
+const updateUI = (id, advice) => {
+  adviceNum.textContent = `#${id}`;
+  adviceContent.textContent = `${advice}`;
+};
+btn.addEventListener('click', () => {
+  fetchAdvice().then((data) => {
+    const { slip } = data;
+    const { id, advice } = slip;
+    updateUI(id, advice);
+  });
+});
