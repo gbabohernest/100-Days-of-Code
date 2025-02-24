fc56bfb2ac1d7fd09a3bb8920cec8590305faa16
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-02-24 05:36:51 +0300

    Return  an encoded wikipedia api search uri

--- Practice Projects/find-me/js/helperFunctions.js
+++ Practice Projects/find-me/js/helperFunctions.js
@@ -8,6 +8,18 @@ export const getSearchTerm = () => {
 };
 
 /**
+ * Construct a Wikipedia API search URL for a given topic.
+ * @param {string} topic - The search term for Wikipedia.
+ * @returns {string} - The encoded Wikipedia API search URL.
+ */
+const getSearchString = (topic) => {
+  const maxChars = getMaxCharacters();
+  const searchString = `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${topic}&gsrlimit=20&prop=pageimages|extract&exchars=${maxChars}&exintro&explaintext&exlimit=max&format=json&origin=*`;
+
+  return encodeURI(searchString);
+};
+
+/**
  * Return the max character  in the extract of a search term (topic) based on the user's viewport width
  */
 const getMaxCharacters = () => {
